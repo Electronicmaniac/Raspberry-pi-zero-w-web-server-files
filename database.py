96545fe7345da8487591850441f1fd8b9cc36e2f
Electronicmaniac <larshunneman@gmail.com>
2017-12-21 16:47:39 +0100

    modified:   database.py

--- database.py
+++ database.py
@@ -5,28 +5,34 @@ import time
 import datetime
 import glob
 import MySQLdb
+import Adafruit_ADS1x15
 import RPi.GPIO as GPIO #RPi.GPIO wordt nu GPIO genoemd
 from time import strftime
 
+#Create an ADS1115 ADC(16-bit) instance.
+adc = Adafruit_ADS1x15.ADS1115()
+
+#Set the I2C adress
+adc = Adafruit_ADS1x15.ADS1115(address=0x49, busnum=1)
+
 InputPin = 18
-voltage = 0
-current = 0
+voltage = 5.32
+current = 1.65
+GAIN = 1
 
 #Variabelen voor MySQL
 db = MySQLdb.connect(host="localhost", user="root", passwd="Mobility", db="Windturbine_database")
 cur = db.cursor()
 
 GPIO.setmode(GPIO.BCM)
-
 GPIO.setup(InputPin, GPIO.IN)
 
-if GPIO.input(InputPin) == True:
-	voltage = 5
-
-else:
-	voltage = 0
+voltage = adc.read_adc(0, gain=GAIN)
+current = adc.read_adc(1, gain=GAIN)
 
 while True:
+	voltage = float(voltage)
+	current = float(current)
 	datetimeWrite = (time.strftime("%Y-%m-%d") + time.strftime(" %H:%M:%S"))
 	print voltage
 	print current
